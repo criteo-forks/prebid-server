400071598e3b9148b6edaa001189bc0d3fef4aa2
Florent Flament <f.flament@criteo.com>
2018-04-27 12:34:43 +0000

    [Criteo adapter] Implement MakeRequests

---
+++ adapters/criteo/criteo.go
@@ -0,0 +1,134 @@
+package criteo
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/mxmCherry/openrtb"
+	"github.com/prebid/prebid-server/adapters"
+	"github.com/prebid/prebid-server/openrtb_ext"
+)
+
+// TODO: (26/04/2018) The profileId is temporary and should be updated soon
+const CRITEO_URL = "https://bidder.criteo.com/cdb?profileId=154"
+
+type CriteoAdapter struct {}
+
+// Criteo bidder request and response structures
+type CriteoRequest struct {
+	Publisher CriteoPublisher     `json:"publisher"`
+	User      CriteoUser          `json:"user"`
+	Slots     []CriteoRequestSlot `json:"slots"`
+}
+
+type CriteoResponse struct {
+	Slots []CriteoResponseSlot `json:"slots"`
+	Exd   []CriteoExtension    `json:"exd"`
+}
+
+type CriteoPublisher struct {
+	BundleId string `json:"bundleid"`
+}
+
+type CriteoUser struct {
+	DeviceId     string `json:"deviceid"`
+	DeviceOs     string `json:"deviceos"`
+	DeviceIdType string `json:"deviceidtype"`
+}
+
+type CriteoRequestSlot struct {
+	ImpId  string `json:"impid"`
+	ZoneId uint   `json:"zoneid"`
+}
+
+type CriteoResponseSlot struct {
+	ImpId    string  `json:"impid"`
+	ZoneId   uint    `json:"zoneid"`
+	Cpm      float64 `json:"cpm"`
+	Currency string  `json:"currency"`
+	Width    uint    `json:"width"`
+	Height   uint    `json:"height"`
+	Creative string  `json:"creative"`
+}
+
+type CriteoExtension struct {
+	GenImpIds []string              `json:"genimpids"`
+	Slots     []CriteoExtensionSlot `json:"slots"`
+}
+
+type CriteoExtensionSlot struct {
+	GenImpId string `json:"imp_id"`
+	ImpId    string `json:"ad_unid_id`
+	ZoneId   uint   `json:"zone_id"`
+}
+
+func (a *CriteoAdapter) MakeRequests(request *openrtb.BidRequest) ([]*adapters.RequestData, []error) {
+	rqjson, err := json.Marshal(CriteoRequest{
+		Publisher: CriteoPublisher{
+			BundleId: request.App.Bundle,
+		},
+		User: CriteoUser{
+			DeviceId: request.Device.IFA,
+			DeviceOs: request.Device.OS,
+			DeviceIdType: getDeviceType(request.Device.OS),
+		},
+		Slots: getRequestSlots(request.Imp),
+	})
+	if err != nil {
+		return nil, []error{err}
+	}
+
+	rqData := adapters.RequestData{
+		Method: "POST",
+		Uri: CRITEO_URL,
+		Body: rqjson,
+		Headers: http.Header{},
+	}
+	return []*adapters.RequestData{&rqData}, nil
+}
+
+func getDeviceType(os string) string {
+	deviceType := map[string]string{
+		"ios": "idfa",
+		"android": "gaid",
+	}
+
+	dtype, ok := deviceType[os]
+	if !ok {
+		return "unknown_device_type";
+	}
+	return dtype
+}
+
+func getRequestSlots(impressions []openrtb.Imp) []CriteoRequestSlot {
+	var slots []CriteoRequestSlot
+
+	for _, imp := range(impressions) {
+		var bidderExt adapters.ExtImpBidder
+		var criteoExt openrtb_ext.ExtImpCriteo
+
+		// TODO - Handle unmarshalling errors
+		json.Unmarshal(imp.Ext, &bidderExt)
+		json.Unmarshal(bidderExt.Bidder, &criteoExt)
+
+		slots = append(slots, CriteoRequestSlot{
+			ImpId: imp.ID,
+			ZoneId: criteoExt.ZoneId,
+		})
+	}
+	return slots
+}
+
+func (a *CriteoAdapter) MakeBids(
+	internalRequest *openrtb.BidRequest,
+	externalRequest *adapters.RequestData,
+	response *adapters.ResponseData,
+)(
+	[]*adapters.TypedBid,
+	[]error,
+){
+	return nil, nil
+}
+
+func NewCriteoBidder() *CriteoAdapter {
+	return &CriteoAdapter{}
+}
