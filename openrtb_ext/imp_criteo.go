dbf4e87fd4014dd767a5eddd8739b7640c76a9ed
Benjamin <benjamin.chastanier@gmail.com>
2021-03-18 22:03:22 +0100

    New Adapter: Criteo

---
+++ openrtb_ext/imp_criteo.go
@@ -0,0 +1,7 @@
+package openrtb_ext
+
+// ExtImpCriteo defines the contract for bidrequest.imp[i].ext.criteo
+type ExtImpCriteo struct {
+	ZoneID    uint `json:"zoneId"`
+	NetworkID uint `json:"networkId"`
+}
